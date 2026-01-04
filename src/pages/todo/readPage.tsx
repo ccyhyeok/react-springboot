d107f42510b6b86580a1d0e10d421ed130368934
ccyhyeok <calvinchun@naver.com>
2026-01-04 17:33:54 +0900

    20260104 React Router

---
+++ src/pages/todo/readPage.tsx
@@ -0,0 +1,20 @@
+import { useParams } from "react-router";
+
+function ReadPage() {
+
+    //const obj = useParams()
+    //console.log(obj)
+    const {tno} = useParams()
+    console.log(tno)
+
+
+
+    return (  
+        <div className="bg-white w-full">
+          <div className="text-4xl">Todo Read Page  {tno}</div>
+        </div>
+
+    );
+}
+
+export default ReadPage;
